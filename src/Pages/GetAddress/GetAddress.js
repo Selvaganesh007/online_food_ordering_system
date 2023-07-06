1feed7821dd3642aa159b280207230fe6ba340e6
pradeepAK07 <pradeep07ak@gmail.com>
2023-07-06 17:26:07 +0530

    address page added

--- src/Pages/GetAddress/GetAddress.js
+++ src/Pages/GetAddress/GetAddress.js
@@ -7,18 +7,32 @@ const GetAddress = () => {
       <div className="address-card">
         <div className="address-head">
           <h3>User name</h3>
+          <div className="header-aside">
+            <p>Contact</p>
+            <p>Already have a account login</p>
+          </div>
+          <input
+            className="input-email"
+            type="email"
+            placeholder="Enter the email"
+          />
+          <br />
           <p>If you want to change you Details you can change below</p>
         </div>
         <div className="address-body">
-          <input className="input-address" />
+          <p className="body-header">Shipping address</p>
+          <input className="input-address" placeholder="Enter Country" />
           <br />
-          <input className="input-address" />
+          <input className="input-address-firstname" placeholder="First Name" />
+          <input className="input-address-lastname" placeholder="Last Name" />
           <br />
-          <input className="input-address" />
+          <input className="input-address" placeholder="Enter Address" />
           <br />
-          <input className="input-address" />
-          <br />
-          <input className="input-address" />
+          <input className="input-address" placeholder="Enter phone numer" />
+        </div>
+        <div className="div-btn">
+          <button className="back-btn">Return To Cart</button>
+          <button className="go-btn">Continue to buy</button>
         </div>
       </div>
     </div>
